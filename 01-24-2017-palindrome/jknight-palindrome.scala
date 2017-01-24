074832d71a54e96e1277f67beaffcc01cbd6e657
Jeff Knight <jknight@opentable.com>
2017-01-24 12:32:48 -0800

    allow for sentences

--- 01-24-2017-palindrome/jknight-palindrome.scala
+++ 01-24-2017-palindrome/jknight-palindrome.scala
@@ -1,8 +1,17 @@
 object Palindrome extends App {
-  def isPalindrome(str: List[Char]): Boolean = str.reverse zip str forall {
-    case (left, right) => left == right
+  val pattern = """([a-zA-Z0-9])""".r
+
+  def isPalindrome(str: List[Char]): Boolean = {
+    val filteredString = str collect {
+      case pattern(x) => x.toLower
+    }
+
+    filteredString.reverse zip filteredString forall {
+      case (left, right) => left == right
+    }
   }
 
   assert(isPalindrome("racecar".toList))
   assert(!isPalindrome("random".toList))
+  assert(isPalindrome("A man, a plan, a canal, Panama!".toList))
 }
