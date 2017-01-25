dd96b885a514da6a9e39dde5b046f74dc1e0906b
Jeff Knight <jknight@opentable.com>
2017-01-24 21:58:08 -0800

    adding ad-hoc polymorphism so palindrome can be used on both numeric types and strings

--- 01-24-2017-palindrome/jknight-palindrome.scala
+++ 01-24-2017-palindrome/jknight-palindrome.scala
@@ -1,17 +1,36 @@
+trait PalindromeFilter[T] extends Function[List[T], List[T]]
+
+object PalindromeFilter {
+  implicit def toNumericEvidence[T : Numeric]: PalindromeFilter[T] = new PalindromeFilter[T] {
+    def apply(v1: List[T]) = v1
+  }
+
+  implicit val charEvidence: PalindromeFilter[Char] = new PalindromeFilter[Char] {
+    private[this] val Pattern = """([a-zA-Z0-9])""".r
+
+    def apply(v1: List[Char]) = v1 collect { case Pattern(x) => x.toLower }
+  }
+}
+
 object Palindrome extends App {
-  val pattern = """([a-zA-Z0-9])""".r
+  /**
+    * Ad-hoc polymorphism allows validating palindromes for types other than List[Char]
+    */
+  def isPalindrome[T](pattern: List[T])(implicit filter: PalindromeFilter[T]): Boolean = {
+    val filteredPattern = filter(pattern)
 
-  def isPalindrome(str: List[Char]): Boolean = {
-    val filteredString = str collect {
-      case pattern(x) => x.toLower
-    }
-
-    filteredString.reverse zip filteredString forall {
+    filteredPattern.reverse zip filteredPattern forall {
       case (left, right) => left == right
     }
   }
 
   assert(isPalindrome("racecar".toList))
-  assert(!isPalindrome("random".toList))
   assert(isPalindrome("A man, a plan, a canal, Panama!".toList))
+  assert(!isPalindrome("random".toList))
+
+  assert(isPalindrome(List(1, 2, 3, 4, 3, 2, 1)))
+  assert(!isPalindrome(List(1, 2, 3, 4, 5)))
+
+  assert(isPalindrome(List(1L, 2L, 3L, 4L, 3L, 2L, 1L)))
+  assert(!isPalindrome(List(1L, 2L, 3L, 4L, 5L)))
 }
