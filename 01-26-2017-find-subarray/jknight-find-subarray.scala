59554e545795f6df2f8a13fd8da27bc734bfbc6b
Jeff Knight <jknight@opentable.com>
2017-01-27 02:07:41 -0800

    fixing an edge condition in the linear solution

--- 01-26-2017-find-subarray/jknight-find-subarray.scala
+++ 01-26-2017-find-subarray/jknight-find-subarray.scala
@@ -6,7 +6,7 @@ object FindSubarray extends App {
   def hasZeroSumSubArray(numbers: List[Int], seen: Set[Int] = Set.empty, sum: Int = 0): Boolean = numbers match {
     case Nil => false
     case 0 :: _ => true
-    case head :: _ if seen.contains(sum + head) => true
+    case head :: _ if seen.contains(sum + head) || sum + head == 0 => true
     case head :: tail => hasZeroSumSubArray(tail, seen + (sum + head), sum + head)
   }
 
@@ -14,4 +14,5 @@ object FindSubarray extends App {
   assert(hasZeroSumSubArray(List(4, 2, 0, 1, 6)))
   assert(!hasZeroSumSubArray(List(-3, 2, 3, 1, 6)))
   assert(hasZeroSumSubArray(List(1, 2, 3, -3, -2, -1)))
+  assert(hasZeroSumSubArray(List(3, -2, -1)))
 }
