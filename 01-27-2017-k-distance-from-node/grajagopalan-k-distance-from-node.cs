54160a8f282fe057cdb17b611026ba6fc9d9b321
Govind <grajagopalan@opentable.com>
2017-01-28 10:22:44 -0800

    rename Tree class to that it reflects a BTree

--- 01-27-2017-k-distance-from-node/grajagopalan-k-distance-from-node.cs
+++ 01-27-2017-k-distance-from-node/grajagopalan-k-distance-from-node.cs
@@ -1,26 +1,26 @@
-    public class TreeNode
+    public class BTreeNode
     {
-        public int Data { get; set; }
-
-        public int Level { get; set; }
-
-        public TreeNode Left { get; set; }
-
-        public TreeNode Right { get; set; }
+            public int Data { get; set; }
+    
+            public int Level { get; set; }
+    
+            public BTreeNode Left { get; set; }
+    
+            public BTreeNode Right { get; set; }
     }
     
     public class NodesAtKDistanceFromRootFinder
     {
-        public static List<TreeNode> Find(TreeNode root, int k)
+        public static List<BTreeNode> Find(BTreeNode root, int k)
         {
-            var nodesAtDistanceK = new List<TreeNode>();
+            var nodesAtDistanceK = new List<BTreeNode>();
 
             if (root == null || k < 0)
             {
                 return nodesAtDistanceK;
             }
 
-            var queue = new Queue<TreeNode>();
+            var queue = new Queue<BTreeNode>();
             root.Level = 0;
             queue.Enqueue(root);
 
@@ -55,61 +55,61 @@
     }
     
     public class NodesAtKDistanceFromRootFinderTests
-    {
-        [TestFixture]
-        public class FindTests
-        {
-            private TreeNode _tree;
-
-            [SetUp]
-            public void TestSetup()
-            {
-                _tree = new TreeNode
-                {
-                    Data = 1,
-                    Left = new TreeNode
-                    {
-                        Data = 2,
-                        Left = new TreeNode() { Data = 4 },
-                        Right = new TreeNode() { Data = 5 }
-                    },
-                    Right = new TreeNode
-                    {
-                        Data = 3,
-                        Left = new TreeNode() { Data = 8 }
-                    }
-                };
-            }
-
-            [Test]
-            public void PositiveCases()
-            {
-                var distance0 = NodesAtKDistanceFromRootFinder.Find(_tree, 0);
-                Assert.AreEqual(1, distance0.Count);
-                Assert.AreEqual(1, distance0[0].Data);
-
-                var distance1 = NodesAtKDistanceFromRootFinder.Find(_tree, 1);
-                Assert.AreEqual(2, distance1.Count);
-                Assert.AreEqual(2, distance1[0].Data);
-                Assert.AreEqual(3, distance1[1].Data);
-
-                var distance2 = NodesAtKDistanceFromRootFinder.Find(_tree, 2);
-                Assert.AreEqual(3, distance2.Count);
-                Assert.AreEqual(4, distance2[0].Data);
-                Assert.AreEqual(5, distance2[1].Data);
-                Assert.AreEqual(8, distance2[2].Data);
-            }
-
-            [Test]
-            public void ErrorCases()
-            {
-                var distance0 = NodesAtKDistanceFromRootFinder.Find(_tree, -1);
-                Assert.AreEqual(0, distance0.Count);
-
-                TreeNode nullTree = null;
-                var distanceNull = NodesAtKDistanceFromRootFinder.Find(nullTree, 2);
-                Assert.AreEqual(0, distanceNull.Count);
-            }
-
-        }
+       {
+           [TestFixture]
+           public class FindTests
+           {
+               private BTreeNode _tree;
+   
+               [SetUp]
+               public void TestSetup()
+               {
+                   _tree = new BTreeNode
+                   {
+                       Data = 1,
+                       Left = new BTreeNode
+                       {
+                           Data = 2,
+                           Left = new BTreeNode() { Data = 4 },
+                           Right = new BTreeNode() { Data = 5 }
+                       },
+                       Right = new BTreeNode
+                       {
+                           Data = 3,
+                           Left = new BTreeNode() { Data = 8 }
+                       }
+                   };
+               }
+   
+               [Test]
+               public void PositiveCases()
+               {
+                   var distance0 = NodesAtKDistanceFromRootFinder.Find(_tree, 0);
+                   Assert.AreEqual(1, distance0.Count);
+                   Assert.AreEqual(1, distance0[0].Data);
+   
+                   var distance1 = NodesAtKDistanceFromRootFinder.Find(_tree, 1);
+                   Assert.AreEqual(2, distance1.Count);
+                   Assert.AreEqual(2, distance1[0].Data);
+                   Assert.AreEqual(3, distance1[1].Data);
+   
+                   var distance2 = NodesAtKDistanceFromRootFinder.Find(_tree, 2);
+                   Assert.AreEqual(3, distance2.Count);
+                   Assert.AreEqual(4, distance2[0].Data);
+                   Assert.AreEqual(5, distance2[1].Data);
+                   Assert.AreEqual(8, distance2[2].Data);
+               }
+   
+               [Test]
+               public void ErrorCases()
+               {
+                   var distance0 = NodesAtKDistanceFromRootFinder.Find(_tree, -1);
+                   Assert.AreEqual(0, distance0.Count);
+   
+                   BTreeNode nullTree = null;
+                   var distanceNull = NodesAtKDistanceFromRootFinder.Find(nullTree, 2);
+                   Assert.AreEqual(0, distanceNull.Count);
+               }
+   
+           }
     }
