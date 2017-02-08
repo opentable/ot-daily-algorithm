6ac9b682c6fc356993b3f9979bfe48c779742767
Jonathan Schapiro <jschapiro@opentable.com>
2017-02-08 09:16:30 -0800

    update validate-bst

--- 2017-02-06-validate-bst/jschapiro-validate-bst.js
+++ 2017-02-06-validate-bst/jschapiro-validate-bst.js
@@ -1,21 +1,57 @@
-function validateBST(root, parent, left = true) {
-  if (!root) {
-    return true;
-  }
-  
-  if (!parent) {
-    parent = root;  
-  }
-  
-  let isValid = false;
-  
-  if (left) {
-    isValid = root.val <= parent.val;
-  }  else {
-    isValid = root.val >= parent.val;
+function min(node, currMin) {
+  if (!node) {
+    return currMin;
   }
 
-  return isValid ?
-    validateBST(root.left, root, true) && validateBST(root.right, root, false) :
-    false;
+  if (!currMin || node.val <= currMin) {
+    currMin = node.val;
+  }
+
+  let leftMin = min(node.left, currMin);
+  let rightMin = min(node.right, currMin);
+
+  if (leftMin < rightMin) {
+    currMin = leftMin;
+  } else {
+    currMin = rightMin;
+  }
+
+  return currMin;
+}
+
+function max(node, currMax) {
+  if (!node) {
+    return currMax;
+  }
+  
+  if (!currMax || node.val >= currMax){
+    currMax = node.val;
+  }
+  
+  const leftMax = max(node.left, currMax);
+  const rightMax = max(node.right, currMax);
+  
+  if (leftMax > rightMax) {
+    currMax = leftMax;
+  } else {
+    currMax = rightMax;
+  }
+
+  return currMax;
+}
+
+function validateBST(node) {
+  if (!node) {
+    return true;
+  }
+
+  if (node.left &&  max(node.left) > node.val) {
+    return false;
+  }
+
+  if (node.right && min(node.right) < node.val) {
+    return false;
+  }
+
+  return validateBST(node.left) && validateBST(node.right);
 }
