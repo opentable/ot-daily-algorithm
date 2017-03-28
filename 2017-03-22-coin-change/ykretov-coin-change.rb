9bac7d09b5dd5000c0af54f5ae021359d24869fe
Yuri Kretov <ykretov@opentable.com>
2017-03-27 21:19:34 -0700

    completely refactored...

--- 2017-03-22-coin-change/ykretov-coin-change.rb
+++ 2017-03-22-coin-change/ykretov-coin-change.rb
@@ -2,86 +2,66 @@
 
 # Main class - to do the calculations
 class MakeChange
-  attr_reader :coins, :amount
-  attr_accessor :counter, :coins_set
+  attr_reader :coins, :amount, :debug
 
-  def initialize(coins, amount)
-    @coins = coins.sort.uniq
+  def initialize(coins, amount, debug)
+    @coins = coins.sort.uniq.reverse
     @amount = amount
-    @counter = 0
-    @coins_set = init_coins_set(coins)
-  end
-
-  def init_coins_set(coins)
-    zeros = Array.new(coins.length, 0)
-    Hash[coins.zip(zeros)]
-  end
-
-  def callback_when_found
-    @counter += 1
-  end
-
-  def find_all_ways
-    sum = coins_set.inject(0) { |sum, (coin, qty)| sum + (coin*qty) }
-    callback_when_found if sum == amount
-
-  end
-
-  def ways_to_change
-    find_all_ways
-    counter
-  end
-end
-
-# Auxiliary class - to print out test results
-class MakeChangeTest < MakeChange
-  attr_reader :debug
-
-  CURRENCY_SYMBOL = '$'
-
-  def initialize(coins, amount, debug = false)
-    super(coins, amount)
     @debug = debug
-    print_current_state if debug
   end
 
-  def print_current_state
-    puts "DEBUG: coins=#{coins}"
-    puts "DEBUG: amount=#{amount}"
-    puts "DEBUG: counter=#{counter}"
-    puts "DEBUG: coins_set=#{coins_set}"
-  end
-
-  def print_combination
-    print "DEBUG: found #{counter}-th way to split #{CURRENCY_SYMBOL}#{amount}: "
-    change_split = coins_set.map do |coin, count|
-      "(#{CURRENCY_SYMBOL}#{coin} x #{count})"
-    end.join(" + ")
-    puts change_split
+  def print_state(n)
+    if debug && (n > 0)
+      puts "==> amount=#{amount}; coins: #{coins}; combinations: #{n}"
+    end
   end
 
   def print_result
-    n = ways_to_change
+    n = find_all_ways
     puts
-    print "Total number of ways to change #{CURRENCY_SYMBOL}#{amount}"
+    print "Total number of ways to change $#{amount}"
     puts " with coins #{coins} is #{n}"
+    puts
   end
 
-  def callback_when_found
-    super
-    print_combination if debug
+  def find_all_ways
+    return 0 if coins.count == 0
+
+    if amount == 0
+      print_state(1)
+      return 1
+    end
+
+    max_coin = coins[0] # largest coin we have
+    n_cases = amount / max_coin
+    if coins.count == 1
+      n = (n_cases*max_coin) == amount ? 1 : 0
+      print_state(n)
+      return n
+    end
+
+    new_coins = coins.clone
+    new_coins.shift # remove first coin
+    n = 0
+    0.upto(n_cases).each do |qty|
+      new_amount = amount - (max_coin * qty)
+      cnt = MakeChange.new(new_coins, new_amount, debug).find_all_ways
+      n += cnt
+    end
+    print_state(n)
+    return n
   end
 end
 
 def test_results(coins, amount, debug = false)
-  test_change = MakeChangeTest.new(coins, amount, debug)
-  test_change.print_result
+  MakeChange.new(coins, amount, debug).print_result
 end
 
 def main
-  test_results([1, 2, 5, 10], 7, true)
-  # test_results([1, 2, 5, 10], 10)
-  # test_results([1, 2, 5, 10], 50)
+  test_results([2, 3, 5], 8, true)
+  test_results([1, 2, 5, 10], 10, true)
+  test_results([1, 2, 5, 10], 15)
+  test_results([2, 3, 5], 17)
 end
 
 main
